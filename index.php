b2314c1f407c7c7d8f9cb720263a290697fbd963
aditya7395 <aditya7395@gmail.com>
2016-12-11 16:37:56 -0800

    Integrated basic Stripe API

--- index.php
+++ index.php
@@ -14,8 +14,13 @@ echo "<!DOCTYPE html>
 	<input type='text' name='emails'><br>
 	<input type='submit' name='Submit'>
 	</form>
-
-
+	<form name = 'test' action='models/charge.php' method='POST'>
+	  <script
+	    src='https://checkout.stripe.com/checkout.js' class='stripe-button'
+	    data-key='pk_test_mfDmFaNUG5FaS8zNKTg3VKAF'
+	    data-description='Download Script ($15.00)''
+	    data-amount='1500'>
+	  </script>
+	</form>
 </body>
 </html>";
-
