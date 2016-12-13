97289a6d9b33fafa5612304610a9d680879dc5b4
aditya7395 <aditya7395@gmail.com>
2016-12-12 20:18:07 -0800

    Moved database connections

--- models/charge.php
+++ models/charge.php
@@ -24,6 +24,29 @@ try{
   $headers = "From: ".$user_email;
   $sent = mail($friend_email, "Wish Fountain", $message, $headers);
 
+
+  $conn = mysqli_connect("localhost", "root", "");
+  $stmt = mysqli_prepare($conn, "CREATE DATABASE Wishes");
+  mysqli_stmt_execute($stmt);
+  mysqli_select_db($conn, "Wishes");
+  $stmt = mysqli_prepare($conn, "DROP TABLE IF EXISTS Wish");
+  $stmt = mysqli_prepare($conn, "CREATE TABLE Wish(
+    id integer primary key auto_increment,
+    fountain varchar(10) default null,
+    color varchar(10) default null,
+    country varchar(12) default null,
+    wish varchar(100) default null,
+    wishAmount float(10,2) default null);");
+  mysqli_stmt_execute($stmt);
+  $fountain = 'fountain1';
+  $color = 'red';
+  $country = 'india';
+  $wish = 'Peace on Earth';
+  $wishAmount = 1.25;
+
+  $stmt = mysqli_prepare($conn, "INSERT INTO Wish (fountain, color, country, wish, wishAmount) VALUES (?,?,?,?,?)");
+      $stmt->bind_param('ssssd', $fountain, $color, $country, $wish, $wishAmount);
+      mysqli_stmt_execute($stmt);
 }
 //catch the errors in any way you like 
 catch(Stripe_CardError $e) {
